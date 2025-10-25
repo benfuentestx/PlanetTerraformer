ff57aa0dec36c700a6daf0aa31cd136150eed150
Benji <benji@terraform.game>
2025-10-24 21:56:23 -0500

    Remove emissive properties from button materials

--- src/js/rocketScene.js
+++ src/js/rocketScene.js
@@ -88,9 +88,7 @@ export class RocketScene {
             const buttonGeometry = new THREE.SphereGeometry(0.03, 8, 8);
             const colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff8800];
             const buttonMaterial = new THREE.MeshBasicMaterial({
-                color: colors[Math.floor(Math.random() * colors.length)],
-                emissive: colors[Math.floor(Math.random() * colors.length)],
-                emissiveIntensity: 0.8
+                color: colors[Math.floor(Math.random() * colors.length)]
             });
             const button = new THREE.Mesh(buttonGeometry, buttonMaterial);
             button.position.set(
